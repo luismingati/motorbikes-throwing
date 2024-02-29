fd7bdc3843963fa5dfe276477ab4f32adcdb1127
luismingati <luismingati@gmail.com>
2024-02-29 17:37:43 -0300

    email test

--- motorbike-throwing/app/api/send/route.ts
+++ motorbike-throwing/app/api/send/route.ts
@@ -1,10 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { EmailTemplate } from '@/app/components/EmailTemplate';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { firstName } = req.body;
 
@@ -13,8 +13,8 @@ export async function POST(req: NextApiRequest, res: NextApiResponse) {
       from: 'dontreply@gmail.com',
       to: ['luismingati@gmail.com'],
       subject: 'Hello world',
-      // Aqui assumimos que você ajustou a chamada para funcionar com um dos dois: text ou react.
-      text: `Hello, ${firstName}!`, // Exemplo simples usando texto
+      react: EmailTemplate({ firstName }),
+      text: `Hello, ${firstName}!`,
     });
 
     res.status(200).json(data);
