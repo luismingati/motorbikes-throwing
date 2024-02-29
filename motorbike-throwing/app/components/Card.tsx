562ac21c08743a48eb6dc133cc761ed25c8deb38
luismingati <luismingati@gmail.com>
2024-02-29 15:57:55 -0300

    cards almost done

--- motorbike-throwing/app/components/Card.tsx
+++ motorbike-throwing/app/components/Card.tsx
@@ -2,8 +2,52 @@ import React from 'react';
 
 const Card: React.FC = () => {
   return (
-    <div>
-      <p>Aqui eh o card importado!</p>
+    <div className='flex flex-col gap-5 items-center lg:flex-row lg:items-stretch lg:justify-center xl:gap-10'>
+    <div className='w-full max-w-96 shadow-md bg-slate-50 text-black p-3 rounded-lg'>
+      <div className='flex content-between items-center mb-4 gap-4'>
+        <p className='text-2xl '>Equipada com a mais sofisticada tecnologia!</p>
+        <p className='text-6xl'>🏍️</p>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <p className='list-item ml-3'>Sistema de injeçao eletronica</p>
+        <p className='list-item ml-3'>Freio a disco nas 2 rodas</p>
+        <p className='list-item ml-3'>Freios CBS</p>
+        <p className='list-item ml-3'>Painel 100% digital</p>
+        <p className='list-item ml-3'>Carregador de celular</p>
+        <p className='list-item ml-3'>Partida elétrica</p>
+      </div>
+    </div>
+    
+    <div className='w-full max-w-96 shadow-md bg-slate-50 text-black p-3 rounded-lg'>
+      <div className='flex content-between items-center mb-4 gap-4'>
+        <p className='text-2xl '>A <span className='font-bold'>unica</span> concessionaria Shinerai que oferece hoje 1 ano de garantia!</p>
+        <p className='text-6xl'>🤝</p>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <p className='font-bold text-lg'>Na sua compra voce ainda ganha:</p>
+        <p className='list-item ml-3'>1 capacete</p>
+        <p className='list-item ml-3'>Tanque cheio</p>
+        <p className='list-item ml-3'>2 primeiras revisoes de garantia 100% gratis</p>
+        <p className='list-item ml-3'>1 ano de garantia</p>
+      </div>
+    </div>
+
+    <div className='w-full max-w-96 shadow-md bg-slate-50 text-black p-3 rounded-lg'>
+    <div className='flex content-between items-center mb-4 gap-4'>
+      <p className='text-2xl '>Um preco justo que cabe no seu bolso!</p>
+      <p className='text-6xl'>🤑</p>
+    </div>
+    <div className='flex flex-col gap-2'>
+      <p className='text-lg'>Valor de apenas <span className='text-xl font-bold text-green-600'>R$15.490,00</span>!</p>
+      <p>Financiamos em ate 48x sem entrada!</p>
+      <div>
+        <h3>Disponivel nas cores:</h3>
+        <p className='list-item ml-3'>Preto</p>
+        <p className='list-item ml-3'>Vermelho</p>
+        <p className='list-item ml-3'>Cinza</p>
+      </div>
+    </div>
+    </div>
     </div>
   );
 };
--- motorbike-throwing/app/page.tsx
+++ motorbike-throwing/app/page.tsx
@@ -1,28 +1,36 @@
 import Card from "./components/Card"
+import { Roboto } from 'next/font/google'
+
+const inter = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 const Home = () => {
   return (
-    <div className="flex h-screen flex-col">
-      <header className="flex bg-white p-6 text-black">
-        <p>HEADER</p>
-      </header>
+    <main className={inter.className}>
+      <div className="flex h-screen flex-col">
+        <header className="flex bg-white p-6 text-black">
+          <p>HEADER</p>
+        </header>
 
-      <main className="flex flex-1 flex-col p-6">
-        <p>
-          A espera acabou, a campeã de vendas vai voltar e agora você está ainda
-          mais perto de conquistar a sua.
-        </p>
+        <main className="flex flex-1 flex-col p-6 text-white">
+          <p>
+            A espera acabou, a campeã de vendas vai voltar e agora você está ainda
+            mais perto de conquistar a sua.
+          </p>
 
-        <div>DESCRIÇÃO + cards</div>
-        <Card />
-        <div>FORMS </div>
+          <div>DESCRIÇÃO + cards</div>
+          <Card />
+          <div>FORMS </div>
 
-      </main>
+        </main>
 
-      <footer className="flex flex-col bg-white p-6 text-black">
-        <p>Desenvolvido por: </p>
-      </footer>
-    </div>
+        <footer className="flex flex-col bg-white p-6 text-black">
+          <p>Desenvolvido por: </p>
+        </footer>
+      </div>
+    </main>
   );
 }
 
